2a3fe04bb66f8cea5b20ed1487fdc2a156977055
yourunb <bxyura@gmail.com>
2023-10-25 15:56:06 +0300

    add select and delete product

--- components/IShop.js
+++ components/IShop.js
@@ -6,10 +6,34 @@ import IShopProducts from './IShopProducts';
 
 class IShop extends React.Component {
 
+  state = {
+    selectedProduct:null,
+    products:JSON.parse(JSON.stringify(this.props.products)),
+  };
+
+  seclectProduct = (number) => {
+    this.setState( {selectedProduct:number} );
+  }
+
+  deleteProduct = (product) => {
+    for (let i = 0; i < this.state.products.length; i++) {
+      if (this.state.products[i].code === product.props.code) {
+        //delete this.state.products[i];
+        this.state.products.splice(i, 1);
+      }
+    }
+    console.log(this.state.products)
+  }
+
   render() {
 
-    const productsCode=this.props.products.map( v =>
-      <IShopProducts key={v.code} code={v.code} product={v.product} price={v.price} url={v.url} quantity={v.quantity} image={v.image}/>
+    const productsCode=this.state.products.map( v =>
+      <IShopProducts key={v.code}
+      code={v.code} product={v.product} price={v.price} url={v.url} quantity={v.quantity} image={v.image}
+      selectedProduct={this.state.selectedProduct}
+      selectedProductChange={this.seclectProduct}
+      deleteProduct={this.deleteProduct}
+      />
     );
 
     return (
--- components/IShopProducts.js
+++ components/IShopProducts.js
@@ -4,16 +4,24 @@ import './IShopProducts.css';
 
 class IShopProducts extends React.Component {
 
+  checkedProductChanged = () => {
+    this.props.selectedProductChange(this.props.code);
+  };
+
+  deleteProduct = () => {
+    this.props.deleteProduct(this);
+  }
+
   render() {
 
     return (
-      <tr className='IShopProducts'>
+      <tr className={(this.props.selectedProduct!==this.props.code) ? 'IShopProducts' : 'IShopProducts checked'} onClick={this.checkedProductChanged}>
         <td className='IShopProducts__name'>{this.props.product}</td>
         <td><img className='IShopProducts__image' src={this.props.image} /></td>
         <td className='IShopProducts__price'>{this.props.price}</td>
         <td><a href={this.props.url} target='_blank'>{this.props.url}</a></td>
         <td>{this.props.quantity}</td>
-        <td><button id={this.props.code}>Remove</button></td>
+        <td><button onClick={this.deleteProduct}>Remove</button></td>
       </tr>
     );
 

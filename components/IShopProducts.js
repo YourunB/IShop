25e04bce9acc4ec0bb5b362e00a4bb6e44889737
YourunB <bxyura@gmail.com>
2023-10-29 13:06:21 +0300

    feat: add stopPropagation for button remove

--- components/IShopProducts.js
+++ components/IShopProducts.js
@@ -8,19 +8,20 @@ class IShopProducts extends React.Component {
     this.props.selectedProductChange(this.props.code);
   };
 
-  deleteProduct = () => {
+  deleteProduct = event => {
+    event.stopPropagation();
     if (confirm('Are you sure?')) this.props.deleteProduct(this);
   }
 
   render() {
 
     return (
-      <tr className={(this.props.selectedProduct===this.props.code) ? 'IShopProducts checked' : 'IShopProducts'}>
-        <td onClick={this.checkedProductChanged} className='IShopProducts__name'>{this.props.product}</td>
-        <td onClick={this.checkedProductChanged}><img className='IShopProducts__image' src={this.props.image} /></td>
-        <td onClick={this.checkedProductChanged} className='IShopProducts__price'>{this.props.price}</td>
-        <td onClick={this.checkedProductChanged}><a href={this.props.url} target='_blank'>{this.props.url}</a></td>
-        <td onClick={this.checkedProductChanged}>{this.props.quantity}</td>
+      <tr className={(this.props.selectedProduct===this.props.code) ? 'IShopProducts checked' : 'IShopProducts'} onClick={this.checkedProductChanged}>
+        <td className='IShopProducts__name'>{this.props.product}</td>
+        <td><img className='IShopProducts__image' src={this.props.image} /></td>
+        <td className='IShopProducts__price'>{this.props.price}</td>
+        <td><a href={this.props.url} target='_blank'>{this.props.url}</a></td>
+        <td>{this.props.quantity}</td>
         <td><button onClick={this.deleteProduct}>Remove</button></td>
       </tr>
     );
